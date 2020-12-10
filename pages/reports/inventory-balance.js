580134aaf30b3bf6f8a246c3bbb45c59b7bcb999
Greg Leeper <gregleeper@Gregs-MacBook-Pro.local>
2020-12-10 14:55:00 -0600

    needed to go through end of enddate day

--- pages/reports/inventory-balance.js
+++ pages/reports/inventory-balance.js
@@ -42,7 +42,7 @@ const TotalTons = () => {
             },
           },
           ticketFilter: {
-            ticketDate: { le: endDate },
+            ticketDate: { le: moment(endDate).endOf("day") },
           },
           limit: 3000,
         },
